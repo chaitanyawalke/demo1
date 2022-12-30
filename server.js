da1a3077320bd1315ee99567302414d348f97bc1
PrashantChede <chedeprashant2022@gmail.com>
2022-12-30 17:17:25 +0530

    done1

---
+++ server.js
@@ -0,0 +1,13 @@
+const express=require("express");
+const app=express();
+const path=require("path");
+
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get("/",function(req,resp)
+{
+    resp.sendFile(path.join(__dirname,"./index.html"));
+});
+
+app.listen(5000);
+console.log("server is listening on port 5000");
